861f81458909c6693aeca37ecc9dab754e2340ab
Thien Nguyen <thienn@outlook.com>
2021-08-02 20:40:18 +0700

    init project

---
+++ src/main/java/com/nashtech/rookies/java05/AssetManagement/AssetManagementApplication.java
@@ -0,0 +1,13 @@
+package com.nashtech.rookies.java05.AssetManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssetManagementApplication.class, args);
+	}
+
+}
