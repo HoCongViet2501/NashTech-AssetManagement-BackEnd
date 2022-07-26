3919d4ade40568eaa9897e277b6669f77c9000e2
duynguyen2 <duynguyen2@nashtechglobal.com>
2022-07-26 03:48:57 +0000

    Updated HelloController.java

--- src/main/java/com/nashtech/rookies/java05/AssetManagement/controller/HelloController.java
+++ src/main/java/com/nashtech/rookies/java05/AssetManagement/controller/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
   @RequestMapping("/hello")
   public String hello() {
-    return "Hello Java4 - 12355- NashTech";
+    return "Hello Java4 - 123556- NashTech";
   }
 }
