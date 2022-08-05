ca3c92f5374e5dabfdbf98c82bfaf0137ca01a74
vonhu <vonhu.alien@gmail.com>
2022-08-06 00:21:26 +0700

    update assignment response

--- src/main/java/com/nashtech/rookies/java05/AssetManagement/dto/response/AssignmentDetailResponse.java
+++ src/main/java/com/nashtech/rookies/java05/AssetManagement/dto/response/AssignmentDetailResponse.java
@@ -2,36 +2,33 @@ package com.nashtech.rookies.java05.AssetManagement.dto.response;
 
 import java.util.Date;
 
-import com.nashtech.rookies.java05.AssetManagement.model.entity.Asset;
 import com.nashtech.rookies.java05.AssetManagement.model.entity.Assignment;
-import com.nashtech.rookies.java05.AssetManagement.model.entity.Category;
-import com.nashtech.rookies.java05.AssetManagement.model.entity.User;
-import com.nashtech.rookies.java05.AssetManagement.model.enums.UserRole;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 
 @Data
+@AllArgsConstructor
 public class AssignmentDetailResponse {
 	private Long id;
 
 	private String assetCode;
 
 	private String assetName;
-	
+
 	private String assignedTo;
 
 	private String assignedBy;
 
 	private Date assignedDate;
+
+	private String state;
 	
 	private String note;
 
-	private String state;
+	private boolean status;
+
+	private String specification;
 
 	public static AssignmentDetailResponse buildFromAssignment(Assignment assignment) {
 		return new AssignmentDetailResponse(assignment.getId(),
@@ -41,22 +38,9 @@ public class AssignmentDetailResponse {
 				assignment.getCreator().getUserName(),
 				assignment.getAssignedDate(),
 				assignment.getState(),
-				assignment.getNote()
-				);
+				assignment.getNote(),
+				assignment.isStatus(),
+				assignment.getAsset().getSpecification());
 	}
-
-	public AssignmentDetailResponse(Long id, String assetCode, String assetName, String assignedTo, String assignedBy,
-			Date assignedDate, String state,String note) {
-		super();
-		this.id = id;
-		this.assetCode = assetCode;
-		this.assetName = assetName;
-		this.assignedTo = assignedTo;
-		this.assignedBy = assignedBy;
-		this.assignedDate = assignedDate;
-		this.state = state;
-		this.note = note;
-	}
-
 
 }
--- src/main/java/com/nashtech/rookies/java05/AssetManagement/dto/response/AssignmentResponse.java
+++ src/main/java/com/nashtech/rookies/java05/AssetManagement/dto/response/AssignmentResponse.java
@@ -2,17 +2,7 @@ package com.nashtech.rookies.java05.AssetManagement.dto.response;
 
 import java.util.Date;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.nashtech.rookies.java05.AssetManagement.model.entity.Asset;
-import com.nashtech.rookies.java05.AssetManagement.model.entity.Assignment;
-import com.nashtech.rookies.java05.AssetManagement.model.entity.Category;
-import com.nashtech.rookies.java05.AssetManagement.model.entity.User;
-
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Data
 public class AssignmentResponse {
